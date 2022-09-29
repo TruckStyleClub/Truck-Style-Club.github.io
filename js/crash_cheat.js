09e22c5aa0c27fc4cb43573d1f488e2172b83599
TruckStyleClub <836252907@qq.com>
2022-09-29 15:48:35 +0800

    Site updated: 2022-09-29 15:48:28

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,16 +1,17 @@
-var OriginTitle = document.title;
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/joke.ico");
-         document.title = '你不加车队愁啥';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '赶紧给我加车队奥';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
+  if (document.hidden) {
+      $('[rel="icon"]').attr('href', "/joke.ico");
+      document.title = '你不加车队愁啥';
+      clearTimeout(titleTime);
+  }
+  else {
+      $('[rel="icon"]').attr('href', "/favicon.ico");
+      document.title = '赶紧给我加车队奥' + OriginTitle;
+      titleTime = setTimeout(function () {
+          document.title = OriginTitle;
+      }, 2000);
+  }
  });
